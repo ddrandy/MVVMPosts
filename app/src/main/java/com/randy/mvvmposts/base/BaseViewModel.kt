6b50681718b3be79ab75936de08ccf7d21cd0593
ddrandy <dddrandy@gmail.com>
2018-07-13 15:57:28 +0800

    add LiveData and basic model

---
+++ app/src/main/java/com/randy/mvvmposts/base/BaseViewModel.kt
@@ -0,0 +1,11 @@
+package com.randy.mvvmposts.base
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * @author randy
+ * @since 7/13/18 3:51 PM
+ */
+abstract class BaseViewModel: ViewModel(){
+
+}
---
+++ app/src/main/java/com/randy/mvvmposts/model/Post.kt
@@ -0,0 +1,7 @@
+package com.randy.mvvmposts.model
+
+/**
+ * @author randy
+ * @since 7/13/18 3:55 PM
+ */
+data class Post(val userId: Int, val id: Int, val title: String, val body: String)
