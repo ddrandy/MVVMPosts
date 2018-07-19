c2abbb75415b987b487e00dd932afeac0d333c7c
ddrandy <dddrandy@gmail.com>
2018-07-19 10:58:18 +0800

    add lifecycle extension. add base view model

---
+++ app/src/main/java/com/randy/mvvmposts/base/BaseViewModel.kt
@@ -0,0 +1,11 @@
+package com.randy.mvvmposts.base
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * @author randy
+ * @since 7/19/18 10:57 AM
+ */
+abstract class BaseViewModel : ViewModel() {
+
+}
