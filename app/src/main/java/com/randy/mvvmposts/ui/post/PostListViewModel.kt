9a0d0bb7308fb2778c68e3410b201618e23f911d
ddrandy <dddrandy@gmail.com>
2018-07-19 11:40:37 +0800

    add post loading functions

--- app/src/main/java/com/randy/mvvmposts/ui/post/PostListViewModel.kt
+++ app/src/main/java/com/randy/mvvmposts/ui/post/PostListViewModel.kt
@@ -1,7 +1,12 @@
 package com.randy.mvvmposts.ui.post
 
+import android.arch.lifecycle.MutableLiveData
+import android.view.View
 import com.randy.mvvmposts.base.BaseViewModel
 import com.randy.mvvmposts.network.PostApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 /**
@@ -11,4 +16,45 @@ import javax.inject.Inject
 class PostListViewModel : BaseViewModel() {
     @Inject
     lateinit var postApi: PostApi
+    private lateinit var disposable: CompositeDisposable
+    val loadingVisibility = MutableLiveData<Int>()
+
+    init {
+        loadPosts()
+    }
+
+    private fun loadPosts() {
+        disposable.add(
+                postApi.getPosts()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .doOnSubscribe { onRetrievePostListStart() }
+                        .doOnTerminate { onRetrievePostListFinish() }
+                        .subscribe(
+                                { onRetrievePostListSuccess() },
+                                { onRetrievePostListError() }
+                        )
+        )
+    }
+
+    private fun onRetrievePostListStart() {
+        loadingVisibility.value = View.VISIBLE
+    }
+
+    private fun onRetrievePostListFinish() {
+        loadingVisibility.value = View.GONE
+    }
+
+    private fun onRetrievePostListSuccess() {
+
+    }
+
+    private fun onRetrievePostListError() {
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.dispose()
+    }
 }
